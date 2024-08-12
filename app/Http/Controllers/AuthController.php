1530fde2494f4033fdda8bd795172f30ef8a8cea
KyawMinHtway066813 <batman066813@gmail.com>
2024-08-13 01:30:41 +0630

    final

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $registerdData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($registerdData);
+
+        $accessToken = $user->createToken('registration_token')->accessToken;
+
+        return response()->json([
+            'user' => $user, 'access_token' => $accessToken], 201);
+
+    }
+    public function login(Request $request)
+    {
+        $validator = validator($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+
+        if($validator->fails()){
+            return response()->json([
+                'ok' => false,
+                'message' => 'Request did not pass the validation.',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        if(Auth::attempt($validator->validated())){
+            $user = Auth::user();
+            $token = $user->createToken('LaravelPassportToken')->accessToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user
+            ], 200);
+        }else {
+            return response()->json([
+                'error' => "Unauthorized"
+            ], 401);
+        }
+    }
+}
